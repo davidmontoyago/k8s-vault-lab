659bb9af18006bb35722cd7baaacbd19d5192a3f
David Montoya <dmontoya@thoughtworks.com>
2018-11-28 10:36:21 -0600

    log vault root token for demo purposes

--- vault-init/init.sh
+++ vault-init/init.sh
@@ -6,10 +6,14 @@ INITIALIZED=$(curl $VAULT_ADDR/v1/sys/init | jq '.initialized')
 
 if [ "$INITIALIZED" = "false" ]
 then
-  echo "initliazing vault..."
-  UNSEAL_KEY=$(curl --request POST --data '{"secret_shares": 1, "secret_threshold": 1}' $VAULT_ADDR/v1/sys/init | jq '.keys_base64[0]')
+  echo "initializing vault..."
+  INIT_RESPONSE=$(curl --request POST --data '{"secret_shares": 1, "secret_threshold": 1}' $VAULT_ADDR/v1/sys/init)
 
+  UNSEAL_KEY=$(echo $INIT_RESPONSE | jq '.keys_base64[0]')
   echo "unseal key is $UNSEAL_KEY... sshhhhh this is a secret"
+
+  ROOT_TOKEN=$(echo $INIT_RESPONSE | jq '.root_token')
+  echo "root token is $ROOT_TOKEN... this is a secret too!"
 
   echo "unsealing vault..."
   curl --request POST --data '{"key": '$UNSEAL_KEY'}' $VAULT_ADDR/v1/sys/unseal | jq
